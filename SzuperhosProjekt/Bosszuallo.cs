7b55d7b47967c360279adac302372c8f4da0e0f7
TDavid <tomik.david1@gmail.com>
2023-10-29 14:10:04 +0100

    Elso commit

---
+++ SzuperhosProjekt/Bosszuallo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SzuperhosProjekt
+{
+    internal abstract class Bosszuallo : ISzuperhos
+    {
+        double szuperero;
+        bool vanEGyengesege;
+
+        protected Bosszuallo(double szuperero, bool vanEGyengesege)
+        {
+            this.szuperero = szuperero;
+            this.vanEGyengesege = vanEGyengesege;
+        }
+
+        public double Szuperero { get => szuperero; set => szuperero = value; }
+        public bool VanEGyengesege { get => vanEGyengesege; set => vanEGyengesege = value; }
+
+        public abstract bool MegmentiAVilagot();
+
+        public double MekkoraAzEreje()
+        {
+            return this.szuperero;
+        }
+        public bool LegyoziE(ISzuperhos szuperhos)
+        {
+            return true;
+        }
+        public override string ToString()
+        {
+            if (vanEGyengesege == true)
+            {
+                return $"Szupererő:{this.szuperero}; Van Gyengesége";
+            }
+            else
+            {
+                return $"Szupererő:{this.szuperero}; Nincs Gyengesége";
+            }
+        }
+    }
+}
